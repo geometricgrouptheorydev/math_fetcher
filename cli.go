f617cf52939a683178e6c2f943c4abfe6ccf53ab
geometricgrouptheorydev <geometricgrouptheorydev@gmail.com>
2026-02-15 18:37:51 +0300

    feat: custom CLI setup

--- cli.go
+++ cli.go
@@ -1,1 +1,42 @@
-package main 
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	return strings.Fields(strings.ToLower(text))
+}
+
+// exit command
+func commandExit(args []string) error {
+	fmt.Println("Closing the Math Fetcher... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+//help command with args
+func commandHelp(args []string) error {
+	if len(args) == 0 {
+		fmt.Println(`Welcome to the Math Fetcher! Here is a list of the available commands:`)
+		for key := range commands {
+			fmt.Println(key)
+		}
+		fmt.Println("For more details about each command, type help <command-name>")
+	} else if len(args) > 1 {
+		fmt.Println("Too many arguments! Please ask for help for one command at a time.")
+	} else {
+		arg := args[0]
+		command, ok := commands[arg]
+		if !ok {
+			fmt.Println("This command does nothing...")
+			fmt.Println("because it doesn't exist!")
+		} else {
+			fmt.Println("Name: " + command.name)
+			fmt.Println("Description: " + command.description)
+		}
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,48 @@
 package main
 
-def main {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func([]string) error
+}
+
+var commands map[string]cliCommand 
+
+func main() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Math Fetcher",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Helps you understand the CLI",
+			callback:    commandHelp,
+		},
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Math Fetcher > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+		commandName := input[0]
+		args := input[1:]
+		command, ok := commands[commandName]
+		if !ok {
+			fmt.Println("Unknown command")
+		} else {
+			command.callback(args)
+		}
+	}
 }
